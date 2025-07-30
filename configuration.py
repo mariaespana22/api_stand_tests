810cce44d32f22a8e5c0b430f50ee67a5bd4de56
Maria Paz España <maria_espana22@hotmail.com>
2025-07-30 17:45:30 +0900

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://cnt-d9b2c75e-bb41-4908-a75b-d825143ad63d.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
